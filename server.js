7864ffd42b89d6d720d2cd07b5df34749f0704cc
Aviran Levi <aveloofficial@gmail.com>
2019-07-22 18:46:07 +0300

    minor update

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + "/dist/mini-project"));
+app.use(express.static("./dist/mini-project"));
 
-// Send all requests to index.html
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/mini-project/index.html"));
+  res.sendFile(path.join(__dirname, "/dist/mini-project/index.html"));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 8080);
