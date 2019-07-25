7d4129bc84c8547a1240f3c434a0e5b9e1ae3725
Aviran Levi <aveloofficial@gmail.com>
2019-07-25 21:14:45 +0300

    deploy to heroku fix

--- server.js
+++ server.js
@@ -9,4 +9,17 @@ app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "/dist/mini-project/index.html"));
 });
 
+let env = process.env.NODE_ENV || "development";
+
+let forceSSL = (req, res, next) => {
+  if (req.headers["x-forwarded-proto"] !== "https") {
+    return res.redirect(["https://", req.get("Host"), req.url].join(""));
+  }
+  return next();
+};
+
+if (env === "production") {
+  app.use(forceSSL);
+}
+
 app.listen(process.env.PORT || 8080);
